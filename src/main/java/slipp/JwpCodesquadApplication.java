72dc87b53ae9c225f85f264108067d527f6446de
javajigi <javajigi@gmail.com>
2017-07-31 16:27:53 +0900

    initial commit

---
+++ src/main/java/slipp/JwpCodesquadApplication.java
@@ -0,0 +1,12 @@
+package slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpCodesquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpCodesquadApplication.class, args);
+	}
+}
---
+++ src/main/java/slipp/web/WelcomeController.java
@@ -0,0 +1,12 @@
+package slipp.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello World!";
+	}
+}
