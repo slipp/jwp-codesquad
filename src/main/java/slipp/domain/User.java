05cc2d5b13f98e8d0c2046dc07e2a053797265aa
javajigi <javajigi@gmail.com>
2017-08-02 16:20:52 +0900

    사용자 관리 기능 구현

---
+++ src/main/java/slipp/domain/User.java
@@ -0,0 +1,34 @@
+package slipp.domain;
+
+public class User {
+	String userId;
+	String password;
+	String name;
+	String email;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
---
+++ src/main/java/slipp/web/UserController.java
@@ -0,0 +1,37 @@
+package slipp.web;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import slipp.domain.User;
+
+@Controller
+public class UserController {
+	ArrayList<User> users = new ArrayList<>();
+	
+	@GetMapping("/users/{index}")
+	public ModelAndView show(@PathVariable int index) {
+		User user = users.get(0);
+		return null;
+	}
+	
+	@GetMapping("/users")
+	public ModelAndView list() {
+		ModelAndView mav = new ModelAndView("user/list");
+		mav.addObject("users", users);
+		return mav;
+	}
+	
+	@PostMapping("/users")
+	public ModelAndView create(User user) {	
+		users.add(user);
+		System.out.println("size : " + users.size());
+		
+		return new ModelAndView("redirect:/users");
+	}
+}
