e572dedc102164c91656dbdf8c134b82cfd1818e
javajigi <javajigi@gmail.com>
2017-08-11 11:59:39 +0900

    로그인 기능 및 수정 기능 구현

--- src/main/java/slipp/domain/User.java
+++ src/main/java/slipp/domain/User.java
@@ -10,49 +10,78 @@ public class User {
 	@Id
 	@GeneratedValue
 	Long id;
-	
-	@Column(name="u_id", nullable=false, unique=true, length=25)
+
+	@Column(name = "u_id", nullable = false, unique = true, length = 25)
 	String userId;
-		
-	@Column(nullable=false, length=25)
+
+	@Column(nullable = false, length = 25)
 	String password;
 
-	@Column(nullable=false, length=25)
+	@Column(nullable = false, length = 25)
 	String name;
-	
-	@Column(length=50)
+
+	@Column(length = 50)
 	String email;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public boolean update(User user) {
+		if (matchPassword(user.password)) {
+			this.name = user.name;
+			this.email = user.email;
+			
+			return true;
+		}
+		
+		return false;
+	}
+
+	public boolean matchPassword(String password) {
+		return this.password.equals(password);
+	}
+
+	public boolean matchId(Long id) {
+		return this.id.equals(id);
+	}
+
 	
-	
+
 }
--- src/main/java/slipp/web/UserController.java
+++ src/main/java/slipp/web/UserController.java
@@ -1,5 +1,7 @@
 package slipp.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,70 @@ public class UserController {
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("loginedUser");
+		return "redirect:/";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		System.out.println("userId : " + userId + " password : " + password);
+		
+		User dbUser = userRepository.findByUserId(userId);
+		if (dbUser == null) {
+			return "user/login_failed";
+		}
+		
+		if (!dbUser.matchPassword(password)) {
+			return "user/login_failed";
+		}
+		
+		session.setAttribute("loginedUser", dbUser);
+		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}/form")
+	public ModelAndView updateForm(@PathVariable Long id, HttpSession session) {
+		Object tempUser = session.getAttribute("loginedUser");
+		if(tempUser == null) {
+			return new ModelAndView("redirect:/");
+		}
+		
+		User loginedUser = (User)tempUser;
+		if (!loginedUser.matchId(id)) {
+			return new ModelAndView("redirect:/");
+		}
+		
+		User user = userRepository.findOne(id);
+		ModelAndView mav = new ModelAndView("user/updateForm");
+		mav.addObject("user", user);
+		return mav;
+	}
+	
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User user, HttpSession session) {
+		Object tempUser = session.getAttribute("loginedUser");
+		if(tempUser == null) {
+			return "redirect:/";
+		}
+		
+		User loginedUser = (User)tempUser;
+		if (!loginedUser.matchId(id)) {
+			return "redirect:/";
+		}
+		
+		User dbUser = userRepository.findOne(id);
+		
+		if (dbUser.update(user)) {
+			userRepository.save(dbUser);
+		}
+		
+		return "redirect:/users";
+	}
+	
 	
 	@GetMapping("/{id}")
 	public ModelAndView show(@PathVariable Long id) {
